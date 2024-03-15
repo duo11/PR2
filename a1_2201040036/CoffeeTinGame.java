c9a744df814211b78e49d3f2f6b39d392c3ca5b1
duo11 <duong01659818134@gmail.com>
2024-03-15 17:33:23 +0700

    asd

--- a1_2201040036/CoffeeTinGame.java
+++ a1_2201040036/CoffeeTinGame.java
@@ -20,20 +20,25 @@ public class CoffeeTinGame {
 
         for (int i = 0; i < tins.length; i++) {
             char[] tin = tins[i];
-
+            // expected last bean
+            // p0 = green parity /\
+            // (p0=1 -> last=GREEN) /\ (p0=0 -> last=BLUE)
+            // count number of greens
             int greens = 0;
             for (char bean : tin) {
                 if (bean == GREEN)
                     greens++;
             }
-
+            // expected last bean
             final char last = (greens % 2 == 1) ? GREEN : BLUE;
 
             // print the content of tin before the game
             System.out.printf("%nTIN (%d Gs): %s %n", greens, Arrays.toString(tin));
 
-
+            // perform the game
+            // get actual last bean
             char lastBean = tinGame(tin);
+            // lastBean = last \/ lastBean != last
 
             // print the content of tin and last bean
             System.out.printf("tin after: %s %n", Arrays.toString(tin));
@@ -46,95 +51,114 @@ public class CoffeeTinGame {
             }
         }
     }
-
+    
+    //2a. Create BeanBags
     public static char[] BeansBag = new char[30];
 
-    static{
-        for (int i = 0; i <10; i++ ){
+    static {
+        for(int i = 0; i < BeansBag.length-2;i+=3) {
             BeansBag[i] = BLUE;
-        }
-        for (int i = 10; i <20; i++ ){
-            BeansBag[i] = GREEN;
-        }
-        for (int i = 20; i <30; i++ ){
-            BeansBag[i] = REMOVED;
+            BeansBag[i+1] = GREEN;
+            BeansBag[i+2] = REMOVED;
         }
     }
-    public static char tinGame(char[] tin){
-        int numberOfBeans = tin.length;
-
-        while(numberOfBeans>=2){
-            numberOfBeans=0;
-            char bean1 = takeOne(tin);
-            char bean2 = takeOne(tin);
-            updateTin(tin,bean1,bean2);
-            for (char c : tin) {
-                if (c == BLUE || c == GREEN) {
-                    numberOfBeans++;
-                }
-            }
+//2b. Change tinGame and takeOne to public
+    //2h. Procedure tinGame: must be modified to use procedure updateTin
+    public static char tinGame(char[] tin) {
+        while (hasAtLeastTwoBeans(tin)) {
+            char[] twoBeans = takeTwo(tin);
+            char b1 = twoBeans[0];
+            char b2 = twoBeans[1];
+            updateTin(tin, b1, b2); //modified
         }
-        for(int i =0; i< tin.length; i++){
-            if(tin[i]==GREEN||tin[i]==BLUE){
-                return tin[i];
-            }
-        }
-        return NULL;
+        return anyBean(tin);
     }
+    //2e. Procedure takeOne
+    public static char takeOne(char[] tin) {
+        int i = 0;
+        while(tin[i] == REMOVED ) i++;
+        if(i != tin.length){
+            int a = randInt(tin.length);
+            while(tin[a] == REMOVED){
+                a = randInt(tin.length);
+            }
+            char bean = tin[a];
+            tin[a] = REMOVED;
+            return bean;
+        }
+        // no beans left
+        else{
+            return NULL;
+        }
 
-    public static char getBean(char[] beansBag, char beanType){
+    }
+    
+//2c. Create 3 new public procedures: randInt, getBean, updateTin.
+    //2d. Procedure randInt.
+    public static int randInt(int n) {
+        Random rand = new Random();
+        return rand.nextInt(n);
+    }
+    //2f. Procedure getBean
+    public static char getBean(char[] beansBag, char beanType) {
+
         int a = randInt(beansBag.length);
         while(beansBag[a] != beanType){
             a = randInt(beansBag.length);
         }
-        char temp = beansBag[a];
-        beansBag[a]=REMOVED;
-        return temp;
+        char bean = beansBag[a];
+        beansBag[a] = REMOVED;
+        return bean;
     }
-    public static char takeOne(char[] beansBag){
-        int numberOfSpaces = 0;
-        for (char c : beansBag) {
-            if (c==NULL) {
-                numberOfSpaces++;
+    //2g. Procedure updateTin
+    public static void updateTin(char[] tin, char beanOne, char beanTwo) {
+        if (beanOne == beanTwo) {
+            putIn(tin,getBean(BeansBag, BLUE));
+        } else {
+            putIn(tin,GREEN);
+        }
+    }
+    private static char anyBean(char[] tin) {
+        for (char bean : tin) {
+            if (bean != REMOVED) {
+                return bean;
             }
         }
-        if(numberOfSpaces!=beansBag.length){
-            
-            int a = randInt(beansBag.length);
-            
-            while(beansBag[a]==NULL){
-                a = randInt(beansBag.length);
+
+        // no beans left
+        return NULL;
+    }
+
+    private static boolean hasAtLeastTwoBeans(char[] tin) {
+        int count = 0;
+        for (char bean : tin) {
+            if (bean != REMOVED) {
+                count++;
             }
-            
-            char temp = beansBag[a];
-            beansBag[a]= NULL;
-            return temp;
+
+            if (count >= 2) // enough beans
+                return true;
         }
-        else{
-            return NULL;
+
+        // not enough beans
+        return false;
+    }
+    private static char[] takeTwo (char[] tin) {
+        char first = takeOne(tin);
+        char second = takeOne(tin);
+        return new char[]{first,second};
+    }
+    private static void putIn(char[] tin, char bean) {
+        for (int i = 0; i < tin.length; i++) {
+            if (tin[i] == REMOVED) { // vacant position
+                tin[i] = bean;
+                break;
+            }
         }
     }
 
-    public static void updateTin(char[] tin, char bean1, char bean2){
-        if (bean1 == bean2){
-            char newBlueBean = getBean(BeansBag,BLUE);
-            int index = randInt(tin.length);
-            while(tin[index]!=NULL){
-                index = randInt(tin.length);
-            }
-            tin[index] = newBlueBean;
-        }
-        else{
-            int index = randInt(tin.length);
-            while(tin[index]!=NULL){
-                index = randInt(tin.length);
-            }
-            tin[index] = GREEN;
-        }
-    }
 
-    public static int randInt(int n){
-        Random rand = new Random();
-        return rand.nextInt(n);
-    }
+
+
 }
+
